3c3e1fde7f5e7ed2edb7ee091cc9d1958d5bc4b4
alireza <alirezaeshahi18@mail.com>
2024-08-26 22:44:09 +0330

    set up SettingsFragment open on click

--- app/src/main/java/ir/Rahpo/RahpoHesab/MainActivity.java
+++ app/src/main/java/ir/Rahpo/RahpoHesab/MainActivity.java
@@ -1,16 +1,25 @@
 package ir.Rahpo.RahpoHesab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settings = findViewById(R.id.settings);
+        setFragmentOpenOn(settings, new SettingsFragment());
+
 
         init();
 
@@ -24,7 +33,28 @@ public class MainActivity extends AppCompatActivity {
             SharedPreferences.Editor editor = sp.edit();
             String defaultCurrency = getString(R.string.rial);
             editor.putString(Constants.KEY_CURRENCY, defaultCurrency);
+            editor.apply();
         }
 
+    }
+
+    private void setFragmentOpenOn(View view, Fragment fragment) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_container, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if(getSupportFragmentManager().getBackStackEntryCount() < 1) {
+            finishAffinity();
+        }
     }
 }
