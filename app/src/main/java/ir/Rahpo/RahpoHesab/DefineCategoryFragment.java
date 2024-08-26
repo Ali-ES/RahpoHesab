34fb12a63293c50896929765f2bc9b764543b7d4
alireza <alirezaeshahi18@mail.com>
2024-08-26 23:04:36 +0330

    Create DefineCategoryFragment with its layout

---
+++ app/src/main/java/ir/Rahpo/RahpoHesab/DefineCategoryFragment.java
@@ -0,0 +1,21 @@
+package ir.Rahpo.RahpoHesab;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DefineCategoryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_define_category, container, false);
+
+
+
+        return v;
+    }
+}
