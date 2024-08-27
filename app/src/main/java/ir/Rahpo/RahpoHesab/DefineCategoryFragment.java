c3ccf9403c3e3db068978e2c4386f7290fc3c910
alireza <alirezaeshahi18@mail.com>
2024-08-28 00:23:22 +0330

    + implement DefineCategoryFragment completely + database handle for defining categories + handle appropriate response from submit category operation + create necessary string resources for DefineCategoryFragment + create necessary style resources for fragment_define_category.xml + Create some useful methods in ViewHelper class to be also used in other tabs + refactor fragment_settings.xml

--- app/src/main/java/ir/Rahpo/RahpoHesab/DefineCategoryFragment.java
+++ app/src/main/java/ir/Rahpo/RahpoHesab/DefineCategoryFragment.java
@@ -1,5 +1,11 @@
 package ir.Rahpo.RahpoHesab;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +13,102 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static ir.Rahpo.RahpoHesab.ViewHelper.isNull;
+
+import ir.Rahpo.RahpoHesab.Utility.CurrencyFormatter;
 
 public class DefineCategoryFragment extends Fragment {
+    private Context context;
+    private EditText categoryName;
+    private EditText commissionPercent;
+    private EditText processPrice;
+    private TextView processPriceCurrency;
+    private Button submit;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_define_category, container, false);
+        context = requireContext();
+
+        categoryName = v.findViewById(R.id.category_name_et);
+        commissionPercent = v.findViewById(R.id.commission_percent_et);
+        processPrice = v.findViewById(R.id.process_price_et);
+        submit = v.findViewById(R.id.submit_b);
+        processPriceCurrency = v.findViewById(R.id.process_price_currency);
+
+        ViewHelper.setUpCurrencyText(context, processPriceCurrency);
+        ViewHelper.setPercentCheckerForEditText(commissionPercent);
+        ViewHelper.setThousandSeparatorForEditText(processPrice);
 
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSubmit();
+            }
+        });
 
         return v;
+    }
+
+    private void handleSubmit() {
+        String[] errorMessages = {getString(R.string.warn_empty_category_name), getString(R.string.warn_empty_commission_percent), getString(R.string.process_price)};
+        if (!ViewHelper.isNull(context, errorMessages, categoryName, commissionPercent, processPrice)) {
+            if(!queryDuplicateName()) {
+                String categoryNameText = categoryName.getText().toString().strip();
+                String commissionPercentText = commissionPercent.getText().toString();
+                String processPriceText = new CurrencyFormatter().parse(processPrice.getText().toString());
+
+                ContentValues cv = new ContentValues();
+                cv.put("NAME", categoryNameText);
+                cv.put("COMMISSION_PERCENT", commissionPercentText);
+                cv.put("PROCESS_PRICE", processPriceText);
+                try {
+
+                    SQLiteOpenHelper helper = new CategoryDatabase(context);
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    db.insert("CATEGORY", null, cv);
+                    db.close();
+                    helper.close();
+
+                    Toast.makeText(context, getString(R.string.success_defining_category), Toast.LENGTH_SHORT).show();
+                    categoryName.setText("");
+                    commissionPercent.setText("");
+                    processPrice.setText("");
+                } catch (SQLException e) {
+                    Toast.makeText(context, getString(R.string.error_defining_category), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+    private boolean queryDuplicateName() {
+        boolean result;
+        try {
+            String categoryNameText = categoryName.getText().toString().strip();
+
+            SQLiteOpenHelper helper = new CategoryDatabase(context);
+            SQLiteDatabase db = helper.getReadableDatabase();
+            Cursor cursor = db.query("CATEGORY", new String[] {"NAME"} , "NAME = ?", new String[] {categoryNameText}, null, null, null);
+            if(cursor.moveToFirst()) {
+                result = true;
+                Toast.makeText(context, getString(R.string.error_duplicate_category_name), Toast.LENGTH_SHORT).show();
+            } else {
+                result = false;
+            }
+            cursor.close();
+            db.close();
+            helper.close();
+
+        } catch (SQLException e) {
+            result = true;
+            Toast.makeText(context, getString(R.string.error_defining_category), Toast.LENGTH_SHORT).show();
+        }
+        return result;
     }
 }
--- app/src/main/java/ir/Rahpo/RahpoHesab/ViewHelper.java
+++ app/src/main/java/ir/Rahpo/RahpoHesab/ViewHelper.java
@@ -10,8 +10,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import ir.Rahpo.RahpoHesab.Utility.CurrencyFormatter;
+import ir.Rahpo.RahpoHesab.Utility.PercentChecker;
 
 public class ViewHelper {
+    public static final String TAG = "ViewHelper";
+    public static boolean isNull(Context c, String msg, EditText editText) {
+        if(editText.getText().toString().isBlank()) {
+            Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+    public static boolean isNull(Context c, String[] messages, EditText...editTexts) {
+        for(int i = 0; i < editTexts.length; i++) {
+            if(editTexts[i].getText().toString().isBlank()) {
+                Toast.makeText(c, messages[i], Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
+        return false;
+    }
     public static void setUpCurrencyText(Context c, TextView currencyTextView) {
         SharedPreferences sp = c.getSharedPreferences(Constants.PREF_CURRENCY, Context.MODE_PRIVATE);
         currencyTextView.setText(sp.getString(Constants.KEY_CURRENCY, ""));
@@ -35,6 +53,35 @@ public class ViewHelper {
                 String formattedPrice = currencyFormatter.format(editText.getText().toString());
                 editText.setText(formattedPrice);
                 editText.setSelection(formattedPrice.length());
+                editText.addTextChangedListener(this);
+            }
+        });
+    }
+
+    public static void setPercentCheckerForEditText(EditText editText) {
+        PercentChecker percentChecker = new PercentChecker();
+        editText.addTextChangedListener(new TextWatcher() {
+            String previousText = "";
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                editText.removeTextChangedListener(this);
+                String percentText = editText.getText().toString();
+                if(percentChecker.matches(percentText)) {
+                    previousText = percentText;
+                } else {
+                    editText.setText(previousText);
+                    editText.setSelection(previousText.length());
+                }
                 editText.addTextChangedListener(this);
             }
         });
