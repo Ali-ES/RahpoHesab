6bfc7abbbec12cdef8177bf4a412245ebc5ec39e
alireza <alirezaeshahi18@mail.com>
2024-09-01 00:09:08 +0330

    refactor tests as to do not divide by 100

--- app/src/test/java/ir/Rahpo/RahpoHesab/CalculationsTest.java
+++ app/src/test/java/ir/Rahpo/RahpoHesab/CalculationsTest.java
@@ -10,61 +10,61 @@ public class CalculationsTest {
 
     @Test
     public void testMinAllowPriceExample1() {
-        String result = calc.getMinAllowedPrice("5000", "0.8");
-        assertEquals("4,960", result);
+        String result = calc.getMinAllowedPrice("5000", "80");
+        assertEquals("1,000", result);
     }
     @Test
     public void testMinAllowPriceExample2() {
-        String result = calc.getMinAllowedPrice("1000", "0.02");
-        assertEquals("999.8", result);
+        String result = calc.getMinAllowedPrice("1000", "2");
+        assertEquals("980", result);
     }
     @Test
     public void testMinAllowPriceExample3() {
-        String result = calc.getMinAllowedPrice("1237565", "0.215");
-        assertEquals("1,234,904.24", result);
+        String result = calc.getMinAllowedPrice("1237565", "21.5");
+        assertEquals("971,488.52", result);
     }
     @Test
     public void testMinAllowPriceExample4() {
-        String result = calc.getMinAllowedPrice("50000", "0.05");
-        assertEquals("49,975", result);
+        String result = calc.getMinAllowedPrice("50000", "5");
+        assertEquals("47,500", result);
     }
 
     @Test
     public void testMaxAllowPriceExample1() {
-        String result = calc.getMaxAllowedPrice("50000", "0.05");
-        assertEquals("50,025", result);
+        String result = calc.getMaxAllowedPrice("50000", "5");
+        assertEquals("52,500", result);
     }
     @Test
     public void testMaxAllowPriceExample2() {
-        String result = calc.getMaxAllowedPrice("1237565", "0.215");
-        assertEquals("1,240,225.76", result);
+        String result = calc.getMaxAllowedPrice("1237565", "21.5");
+        assertEquals("1,503,641.48", result);
     }
     @Test
     public void testMaxAllowPriceExample3() {
-        String result = calc.getMaxAllowedPrice("10250200", "0.075");
-        assertEquals("10,257,887.65", result);
+        String result = calc.getMaxAllowedPrice("10250200", "7.5");
+        assertEquals("11,018,965", result);
     }
 
 
     @Test
     public void testFinalBuyCostExample1() {
-        String result = calc.getFinalBuyListCost("50000", "2000", "0.08");
-        assertEquals("52,040", result);
+        String result = calc.getFinalBuyListCost("50000", "2000", "8");
+        assertEquals("56,000", result);
     }
     @Test
     public void testFinalBuyCostExample2() {
-        String result = calc.getFinalBuyListCost("1270235", "3405", "0.125");
-        assertEquals("1,275,227.79", result);
+        String result = calc.getFinalBuyListCost("1270235", "3405", "12.5");
+        assertEquals("1,432,419.38", result);
     }
     @Test
     public void testFinalBuyCostExample3() {
-        String result = calc.getFinalBuyListCost("1533", "125", "0.05");
-        assertEquals("1,658.77", result);
+        String result = calc.getFinalBuyListCost("1533", "125", "5");
+        assertEquals("1,734.65", result);
     }
 
     @Test
     public void testNetReceiveExample1() {
-        String result = calc.getNetReceive("780000", "0.08", "75000");
-        assertEquals("696,813.6", result);
+        String result = calc.getNetReceive("780000", "8", "75000");
+        assertEquals("628,860", result);
     }
 }
