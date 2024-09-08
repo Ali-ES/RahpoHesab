1e8d98eff1321a792faeb6c150c31f1468bbfe9c
alireza <alirezaeshahi18@mail.com>
2024-09-09 00:58:42 +0330

    add companies information to the end of SettingsFragment with its necessary responses

--- app/src/main/java/ir/Rahpo/RahpoHesab/Fragments/SettingsFragment.java
+++ app/src/main/java/ir/Rahpo/RahpoHesab/Fragments/SettingsFragment.java
@@ -1,7 +1,10 @@
 package ir.Rahpo.RahpoHesab.Fragments;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
@@ -65,6 +69,40 @@ public class SettingsFragment extends Fragment {
         });
 
 
+        v.findViewById(R.id.contact_us).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(getString(R.string.company_phone)));
+                startActivity(intent);
+            }
+        });
+
+        v.findViewById(R.id.website).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.company_site)));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e){
+                    Log.v(TAG, e.getMessage());
+                }
+            }
+        });
+        v.findViewById(R.id.telegram_support).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.company_telegram)));
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e){
+                    Log.v(TAG, e.getMessage());
+                }
+
+            }
+        });
 
 
         return v;
